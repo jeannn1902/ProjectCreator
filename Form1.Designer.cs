9283255ac380276292892b9052d6a6def3e329e9
jeannn1902 <jpp25413070160236@cecyteh.edu.mx>
2026-06-29 21:27:06 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace ProjectCreator
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
+            lblTema = new Label();
+            cmbTemas = new ComboBox();
+            lblNombre = new Label();
+            txtNombreProyecto = new TextBox();
+            lblVistaPrevia = new Label();
+            lblNombreFinal = new Label();
+            btnCrearProyecto = new Button();
+            lblTitulo = new Label();
+            lblObjetivo = new Label();
+            txtObjetivo = new TextBox();
+            pictureBoxfondo = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxfondo).BeginInit();
+            SuspendLayout();
+            // 
+            // lblTema
+            // 
+            lblTema.AutoSize = true;
+            lblTema.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTema.Location = new Point(208, 85);
+            lblTema.Name = "lblTema";
+            lblTema.Size = new Size(62, 25);
+            lblTema.TabIndex = 0;
+            lblTema.Text = "Tema:";
+            lblTema.Click += Label1_Click_1;
+            // 
+            // cmbTemas
+            // 
+            cmbTemas.Font = new Font("Segoe UI Light", 9F);
+            cmbTemas.FormattingEnabled = true;
+            cmbTemas.Location = new Point(114, 113);
+            cmbTemas.Name = "cmbTemas";
+            cmbTemas.Size = new Size(250, 33);
+            cmbTemas.TabIndex = 1;
+            cmbTemas.SelectedIndexChanged += CmbTemas_SelectedIndexChanged;
+            // 
+            // lblNombre
+            // 
+            lblNombre.AutoSize = true;
+            lblNombre.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            lblNombre.Location = new Point(143, 158);
+            lblNombre.Name = "lblNombre";
+            lblNombre.Size = new Size(193, 25);
+            lblNombre.TabIndex = 2;
+            lblNombre.Text = "Nombre del ejercicio:";
+            // 
+            // txtNombreProyecto
+            // 
+            txtNombreProyecto.Font = new Font("Segoe UI Light", 9F);
+            txtNombreProyecto.Location = new Point(114, 185);
+            txtNombreProyecto.Name = "txtNombreProyecto";
+            txtNombreProyecto.Size = new Size(250, 31);
+            txtNombreProyecto.TabIndex = 3;
+            txtNombreProyecto.TextChanged += TxtNombreProyecto_TextChanged;
+            // 
+            // lblVistaPrevia
+            // 
+            lblVistaPrevia.AutoSize = true;
+            lblVistaPrevia.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            lblVistaPrevia.Location = new Point(181, 319);
+            lblVistaPrevia.Name = "lblVistaPrevia";
+            lblVistaPrevia.Size = new Size(117, 25);
+            lblVistaPrevia.TabIndex = 4;
+            lblVistaPrevia.Text = "Vista previa:";
+            lblVistaPrevia.Click += LblVistaPrevia_Click;
+            // 
+            // lblNombreFinal
+            // 
+            lblNombreFinal.AutoSize = true;
+            lblNombreFinal.Font = new Font("Consolas", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblNombreFinal.ForeColor = Color.MidnightBlue;
+            lblNombreFinal.Location = new Point(139, 345);
+            lblNombreFinal.Name = "lblNombreFinal";
+            lblNombreFinal.Size = new Size(208, 23);
+            lblNombreFinal.TabIndex = 5;
+            lblNombreFinal.Text = "Esperando datos...";
+            lblNombreFinal.Click += LblNombreFinal_Click;
+            // 
+            // btnCrearProyecto
+            // 
+            btnCrearProyecto.BackColor = Color.DodgerBlue;
+            btnCrearProyecto.FlatStyle = FlatStyle.Flat;
+            btnCrearProyecto.Font = new Font("Constantia", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnCrearProyecto.ForeColor = Color.White;
+            btnCrearProyecto.Location = new Point(154, 376);
+            btnCrearProyecto.Name = "btnCrearProyecto";
+            btnCrearProyecto.Size = new Size(170, 35);
+            btnCrearProyecto.TabIndex = 6;
+            btnCrearProyecto.Text = "Crear Proyecto";
+            btnCrearProyecto.UseVisualStyleBackColor = false;
+            btnCrearProyecto.Click += BtnCrearProyecto_Click;
+            // 
+            // lblTitulo
+            // 
+            lblTitulo.Font = new Font("Cooper Black", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblTitulo.Location = new Point(26, 17);
+            lblTitulo.Name = "lblTitulo";
+            lblTitulo.Size = new Size(427, 58);
+            lblTitulo.TabIndex = 7;
+            lblTitulo.Text = "Automatiza la creación de proyectos de C++";
+            lblTitulo.TextAlign = ContentAlignment.MiddleCenter;
+            lblTitulo.Click += Label1_Click_2;
+            // 
+            // lblObjetivo
+            // 
+            lblObjetivo.AutoSize = true;
+            lblObjetivo.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            lblObjetivo.Location = new Point(141, 232);
+            lblObjetivo.Name = "lblObjetivo";
+            lblObjetivo.Size = new Size(197, 25);
+            lblObjetivo.TabIndex = 8;
+            lblObjetivo.Text = "Objetivo del ejercicio:";
+            // 
+            // txtObjetivo
+            // 
+            txtObjetivo.Location = new Point(114, 258);
+            txtObjetivo.Multiline = true;
+            txtObjetivo.Name = "txtObjetivo";
+            txtObjetivo.ScrollBars = ScrollBars.Vertical;
+            txtObjetivo.Size = new Size(250, 60);
+            txtObjetivo.TabIndex = 9;
+            txtObjetivo.TextChanged += TxtObjetivo_TextChanged;
+            // 
+            // pictureBoxfondo
+            // 
+            pictureBoxfondo.BackColor = Color.White;
+            pictureBoxfondo.Dock = DockStyle.Fill;
+            pictureBoxfondo.ImageLocation = "";
+            pictureBoxfondo.Location = new Point(0, 0);
+            pictureBoxfondo.Name = "pictureBoxfondo";
+            pictureBoxfondo.Size = new Size(803, 535);
+            pictureBoxfondo.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBoxfondo.TabIndex = 10;
+            pictureBoxfondo.TabStop = false;
+            pictureBoxfondo.Click += PictureBox1_Click;
+            // 
+            // frmPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.WhiteSmoke;
+            ClientSize = new Size(803, 535);
+            Controls.Add(txtObjetivo);
+            Controls.Add(lblObjetivo);
+            Controls.Add(lblTitulo);
+            Controls.Add(btnCrearProyecto);
+            Controls.Add(lblNombreFinal);
+            Controls.Add(lblVistaPrevia);
+            Controls.Add(txtNombreProyecto);
+            Controls.Add(lblNombre);
+            Controls.Add(cmbTemas);
+            Controls.Add(lblTema);
+            Controls.Add(pictureBoxfondo);
+            Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "frmPrincipal";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "ProjectCreator v1.0";
+            Load += FrmPrincipal_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBoxfondo).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblTema;
+        private ComboBox cmbTemas;
+        private Label lblNombre;
+        private TextBox txtNombreProyecto;
+        private Label lblVistaPrevia;
+        private Label lblNombreFinal;
+        private Button btnCrearProyecto;
+        private Label lblTitulo;
+        private Label lblObjetivo;
+        private TextBox txtObjetivo;
+        private PictureBox pictureBoxfondo;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,218 @@
+using System.Windows.Forms;
+
+namespace ProjectCreator {
+
+    public partial class frmPrincipal : Form {
+
+        private string rutaBase = "";
+        private string rutaPlantilla = "";
+
+        private void CargarConfiguracion() {
+            string rutaConfig =
+            @"C:\Users\jeanc\source\repos\Plantillas\ProjectCreator\config.txt";
+
+            if (!File.Exists(rutaConfig)) {
+                MessageBox.Show("No se encontró el archivo config.txt");
+                Application.Exit();
+                return;
+            }
+
+            string[] lineas = File.ReadAllLines(rutaConfig);
+
+            if (lineas.Length < 2) {
+                MessageBox.Show("El archivo config.txt está incompleto.");
+                Application.Exit();
+                return;
+            }
+
+            rutaBase = lineas[0];
+            rutaPlantilla = lineas[1];
+        }
+
+        public frmPrincipal() {
+            InitializeComponent();
+            CargarConfiguracion();
+        }
+
+        private void Label1_Click(object sender, EventArgs e) {
+        }
+
+        private void Label2_Click(object sender, EventArgs e) {
+        }
+
+        private void Label1_Click_1(object sender, EventArgs e) {
+        }
+
+        private void LblVistaPrevia_Click(object sender, EventArgs e) {
+        }
+
+        private void TxtNombreProyecto_TextChanged(object sender, EventArgs e) {
+            ActualizarVistaPrevia();
+            ValidarFormulario();
+        }
+
+        private void FrmPrincipal_Load(object sender, EventArgs e) {
+            cmbTemas.Items.Add("01_Variables");
+            cmbTemas.Items.Add("02_Condicionales");
+            cmbTemas.Items.Add("03_Ciclos");
+            cmbTemas.Items.Add("04_Funciones");
+            cmbTemas.Items.Add("05_Strings");
+            cmbTemas.Items.Add("06_Arrays");
+            cmbTemas.Items.Add("07_Structs");
+            cmbTemas.Items.Add("08_Vectores");
+            cmbTemas.Items.Add("09_Archivos");
+            cmbTemas.Items.Add("10_POO");
+            cmbTemas.SelectedIndex = 0;
+
+            btnCrearProyecto.Enabled = false;
+
+            string rutaImagen =
+            @"C:\Users\jeanc\source\repos\Plantillas\ProjectCreator\Recursos\FondodeProyectoAutocpp.png";
+
+            if (File.Exists(rutaImagen)) {
+                pictureBoxfondo.Image = Image.FromFile(rutaImagen);
+                pictureBoxfondo.SendToBack();
+            } else {
+                MessageBox.Show("No se encontró la imagen:\n" + rutaImagen);
+            }
+        }
+
+        private void ActualizarVistaPrevia() {
+            if (cmbTemas.SelectedItem == null || txtNombreProyecto.Text.Trim() == "") {
+                lblNombreFinal.Text = "Esperando datos...";
+                return;
+            }
+
+            string temaSeleccionado = cmbTemas.Text;
+            string rutaTema = Path.Combine(rutaBase, temaSeleccionado);
+            int siguienteNumero = 1;
+
+            if (Directory.Exists(rutaTema)) {
+                string[] carpetas = Directory.GetDirectories(rutaTema);
+                siguienteNumero = carpetas.Length + 1;
+            }
+
+            string numeroFormateado = siguienteNumero.ToString("00");
+            lblNombreFinal.Text = numeroFormateado + "_" + txtNombreProyecto.Text.Trim();
+        }
+
+        private void ValidarFormulario() {
+            btnCrearProyecto.Enabled = txtNombreProyecto.Text.Trim() != "" &&
+            txtObjetivo.Text.Trim() != "";
+        }
+
+        private void CmbTemas_SelectedIndexChanged(object sender, EventArgs e) {
+            ActualizarVistaPrevia();
+        }
+
+        private void BtnCrearProyecto_Click(object sender, EventArgs e) {
+            string temaSeleccionado = cmbTemas.Text;
+            string nombreProyecto = lblNombreFinal.Text;
+            string nombreUsuario = txtNombreProyecto.Text.Trim();
+
+            if (nombreUsuario == "") {
+                MessageBox.Show("Escribe un nombre para el proyecto.");
+                return;
+            }
+
+            char[] caracteresInvalidos = Path.GetInvalidFileNameChars();
+
+            if (nombreUsuario.IndexOfAny(caracteresInvalidos) >= 0) {
+                MessageBox.Show("El nombre contiene caracteres no válidos.");
+                return;
+            }
+
+            string rutaProyecto = Path.Combine(rutaBase, temaSeleccionado, nombreProyecto);
+
+            if (Directory.Exists(rutaProyecto)) {
+                MessageBox.Show("El proyecto ya existe.");
+                return;
+            }
+
+            Directory.CreateDirectory(rutaProyecto);
+
+            // Copiar archivos de la plantilla al nuevo proyecto
+            foreach (string archivo in Directory.GetFiles(rutaPlantilla)) {
+                string nombreArchivo = Path.GetFileName(archivo);
+                string destino = Path.Combine(rutaProyecto, nombreArchivo);
+                File.Copy(archivo, destino);
+            }
+
+            // Renombrar archivos que contienen "00_Plantilla" en su nombre
+            foreach (string archivo in Directory.GetFiles(rutaProyecto)) {
+                string nombreArchivo = Path.GetFileName(archivo);
+
+                if (nombreArchivo.Contains("00_Plantilla")) {
+                    string nuevoNombre = nombreArchivo.Replace("00_Plantilla", nombreProyecto);
+                    string nuevaRuta = Path.Combine(rutaProyecto, nuevoNombre);
+                    File.Move(archivo, nuevaRuta);
+                }
+            }
+
+
+            // Reemplazar "00_Plantilla" en el contenido de los archivos
+            foreach (string archivo in Directory.GetFiles(rutaProyecto)) {
+                string extension = Path.GetExtension(archivo);
+
+                if (extension == ".sln" || extension == ".vcxproj" || extension == ".filters" ||
+                    extension == ".cpp" || extension == ".user") {
+
+                    string contenido = File.ReadAllText(archivo);
+                    contenido = contenido.Replace("00_Plantilla", nombreProyecto);
+                    File.WriteAllText(archivo, contenido);
+                }
+            }
+
+            //Crear el archivo README.md
+            string contenidoReadme =
+            $@"# {nombreProyecto}
+
+            ## Tema
+            {temaSeleccionado}
+
+            ## Objetivo
+            {txtObjetivo.Text.Trim()}
+
+            ## Fecha de creación
+            {DateTime.Now:dd/MM/yyyy}
+
+            ## Descripción
+            Ejercicio creado automáticamente mediante ProjectCreator.";
+
+            string rutaReadme = Path.Combine(rutaProyecto, "README.md");
+
+            File.WriteAllText(rutaReadme, contenidoReadme);
+            string rutaSolucion = Path.Combine(rutaProyecto, nombreProyecto + ".sln");
+
+            if (!File.Exists(rutaSolucion)) {
+                MessageBox.Show("No se encontró la solución:\n" + rutaSolucion);
+                return;
+            }
+
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo() {
+                FileName = rutaSolucion,
+                UseShellExecute = true
+            });
+
+            txtNombreProyecto.Clear();
+            txtObjetivo.Clear();
+
+            MessageBox.Show("Proyecto creado correctamente.");
+
+        }
+
+        private void LblNombreFinal_Click(object sender, EventArgs e) {
+        }
+
+        private void Label1_Click_2(object sender, EventArgs e) {
+        }
+
+        private void TxtObjetivo_TextChanged(object sender, EventArgs e) {
+            ValidarFormulario();
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e) {
+
+        }
+    }
+}
